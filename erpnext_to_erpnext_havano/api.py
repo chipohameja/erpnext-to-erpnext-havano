895dd967bd0585786c96bb0027dbd6ad6a711fb1
KelvinAaron <k.aaron-onu@alustudent.com>
2025-09-26 15:40:33 +0000

    Removed the class

--- erpnext_to_erpnext_havano/api.py
+++ erpnext_to_erpnext_havano/api.py
@@ -7,52 +7,40 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class ERPNextToERPNextHavanoAPI:
-	cloud_url = os.getenv("CLOUD_URL")
-	base_url = os.getenv("BASE_URL")
 
-	headers = {
-		"Authorization": f'token {os.getenv("API_KEY")}:{os.getenv("API_SECRET")}'
-	}
+cloud_url = os.getenv("CLOUD_URL")
+base_url = os.getenv("BASE_URL")
 
-
-	synced = {
-		"custom_synced": 1
-		}
-	users = {}
-	companies = {}
-	accounts = {}
-	customers = {}
-	item_groups = {}
-	warehouses = {}
-	items = {}
+headers = {
+	"Authorization": f'token {os.getenv("API_KEY")}:{os.getenv("API_SECRET")}'
+}
 
 
-	@frappe.whitelist()
-	def sync_data(self, doctype):
-		# Fetch and sync item groups
-		try:
-			self.items = requests.get(f'{self.cloud_url}{doctype}?fields=["*"]', headers=self.headers).json()
-			for item in self.items["data"]:
-				if item["custom_synced"] == 0:
-					if not frappe.db.exists(doctype, item["name"]):
-						item["custom_synced"] = 1
-						# data = json.loads(item) if isinstance(item, str) else item
-						item["doctype"] = doctype
-						new_item = frappe.get_doc(item)
-						new_item.insert()
-						frappe.db.commit()
-					
-						put_response = requests.post(
-							f"{self.cloud_url}api/method/erpnext_to_erpnext_havano.api.update_item",
-							json={"doc": doctype, "name": item['name']}
-							)					
-						frappe.errprint(put_response.text)
-			frappe.msgprint(f"{doctype} synced.")
-		except Exception as e:
-			frappe.errprint(e)
+@frappe.whitelist()
+def sync_data(self, doctype):
+	# Fetch and sync item groups
+	try:
+		items = requests.get(f'{cloud_url}{doctype}?fields=["*"]', headers=headers).json()
+		for item in items["data"]:
+			if item["custom_synced"] == 0:
+				if not frappe.db.exists(doctype, item["name"]):
+					item["custom_synced"] = 1
+					# data = json.loads(item) if isinstance(item, str) else item
+					item["doctype"] = doctype
+					new_item = frappe.get_doc(item)
+					new_item.insert()
+					frappe.db.commit()
+				
+					put_response = requests.post(
+						f"{cloud_url}api/method/erpnext_to_erpnext_havano.api.update_item",
+						json={"doc": doctype, "name": item['name']}
+						)					
+					frappe.errprint(put_response.text)
+		frappe.msgprint(f"{doctype} synced.")
+	except Exception as e:
+		frappe.errprint(e)
 
-	@frappe.whitelist()
-	def update_item(doc, name):
-		frappe.db.set_value(doc, name, "custom_synced", 1)
-		frappe.db.commit()
+@frappe.whitelist()
+def update_item(doc, name):
+	frappe.db.set_value(doc, name, "custom_synced", 1)
+	frappe.db.commit()
